6b492301319ee4933380d4d50c45622927335d97
1256639 <167916944+1256639@users.noreply.github.com>
2025-06-23 10:23:41 -0400

    JavaScript exercises finished

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDividableByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDividableByFourHundred = year % 400 === 0;
 
+    if (isYearDividableByFour && !isCentury || isYearDividableByFourHundred){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
