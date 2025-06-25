91cfe069a2cb55e284fe543bc5b8930d29981afd
1256639 <167916944+1256639@users.noreply.github.com>
2025-06-25 14:39:05 -0400

    JavaScript exercises finished

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if (!death){
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfBirth);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentAge : oldest;
+    });
 };
 
 // Do not edit below this line
